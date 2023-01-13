a1bf7f0a66f8cd2ddb19ee15034800936980b1ab
antonlouf <antonlouf@live.be>
2023-01-13 13:47:15 +0100

    Unit test fix

--- app/src/androidTest/java/com/example/cryptocurrencyapp/data/repository/CoinRepositoryImplTest.kt
+++ app/src/androidTest/java/com/example/cryptocurrencyapp/data/repository/CoinRepositoryImplTest.kt
@@ -1,16 +1,23 @@
 package com.example.cryptocurrencyapp.data.repository
 
 import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.cryptocurrencyapp.data.db.CryptoDao
 import com.example.cryptocurrencyapp.data.db.CryptoDatabase
 import com.example.cryptocurrencyapp.domain.model.Coin
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class CoinRepositoryImplTest {
+@RunWith(AndroidJUnit4ClassRunner::class)
+class CoinRepositoryImplTest  {
 
     private lateinit var dao: CryptoDao
     private lateinit var db: CryptoDatabase
@@ -19,16 +26,23 @@ class CoinRepositoryImplTest {
 
     @Before
     fun setup() {
-        dao = CryptoDatabase.getInstance(Application()).getCryptoDao()
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, CryptoDatabase::class.java).build()
+        dao = db.getCryptoDao()
+
         coin1 = Coin(1, "1", true, "bitcoin", 1, "btc")
         coin2 = Coin(2, "2", true, "ether", 1, "eth")
     }
 
+    @After
+    fun closeDB(){
+        db.close()
+    }
 
     @Test
     fun insertTest() = runBlocking{
         dao.insert(coin1)
         val coinById = dao.findCoinByCId(1)
-        assertEquals(coinById, CoreMatchers.equalTo(coin2))
+        assertEquals(coinById, coin2)
     }
 }
--- app/src/test/java/com/example/cryptocurrencyapp/data/repository/CoinRepositoryImplTest.kt
+++ app/src/test/java/com/example/cryptocurrencyapp/data/repository/CoinRepositoryImplTest.kt
@@ -4,17 +4,22 @@ import android.app.Application
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.cryptocurrencyapp.data.db.CryptoDao
 import com.example.cryptocurrencyapp.data.db.CryptoDatabase
 import com.example.cryptocurrencyapp.domain.model.Coin
+import junit.framework.TestCase
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers
 import org.junit.After
+import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class CoinRepositoryImplTest {
+@RunWith(AndroidJUnit4::class)
+class CoinRepositoryImplTest : TestCase(){
 
     private lateinit var dao: CryptoDao
     private lateinit var db: CryptoDatabase
@@ -23,16 +28,26 @@ class CoinRepositoryImplTest {
 
     @Before
     fun setup() {
-        dao = CryptoDatabase.getInstance(Application()).getCryptoDao()
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, CryptoDatabase::class.java).build()
+        dao = db.getCryptoDao()
+
         coin1 = Coin(1, "1", true, "bitcoin", 1, "btc")
         coin2 = Coin(2, "2", true, "ether", 1, "eth")
+
+    }
+
+    @After
+    fun closeDB(){
+        db.close()
     }
 
 
     @Test
-     fun insertTest() = runBlocking{
+    fun insertTest() = runBlocking{
         dao.insert(coin1)
         val coinById = dao.findCoinByCId(1)
-        assertEquals(coinById, CoreMatchers.equalTo(coin2))
+        Assert.assertEquals(coinById, CoreMatchers.equalTo(coin2))
     }
+
 }
